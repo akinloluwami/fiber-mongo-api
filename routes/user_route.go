2fd6b6ade20a4a3523b7fb09a44e581c648569d3
Akinkunmi <akinkunmiisrael41@gmail.com>
2023-02-02 21:08:53 +0100

    edit a user

--- routes/user_route.go
+++ routes/user_route.go
@@ -10,4 +10,5 @@ import (
 func UserRoute (app *fiber.App){
 app.Post("/user", controllers.CreateUser)
 app.Get("/user/:userId", controllers.GetAUser)
+app.Put("/user/:userId", controllers.EditAUser)
 }

7dfaf235b9bff61f6325932b3a964670b530f184
Akinkunmi <akinkunmiisrael41@gmail.com>
2023-02-02 21:03:35 +0100

    create user route

--- routes/user_route.go
+++ routes/user_route.go
@@ -1,8 +1,12 @@
 package routes
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"fiber-mongo-api/controllers"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 
 func UserRoute (app *fiber.App){
-	
+app.Post("/user", controllers.CreateUser)
 }

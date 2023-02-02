281e887d17d2c0b8f48683185faef297eabede89
Akinkunmi <akinkunmiisrael41@gmail.com>
2023-02-02 20:30:47 +0100

    set up entry point

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"github.com/gofiber/fiber/v2"
+)
+
+func main(){
+	app:=fiber.New()
+	app.Get("/", func(c *fiber.Ctx) error {
+	return c.JSON((&fiber.Map{"data":"Hello from Fiber and MongoDB"}))
+})
+app.Listen(":1600")
+}

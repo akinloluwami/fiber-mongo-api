f2d390a9ce6eac7382e0a42d782f6aa25a7ee480
Akinkunmi <akinkunmiisrael41@gmail.com>
2023-02-02 21:08:13 +0100

    edit a user

--- controllers/user_controller.go
+++ controllers/user_controller.go
@@ -65,3 +65,42 @@ func GetAUser(c *fiber.Ctx) error {
 
     return c.Status(http.StatusOK).JSON(responses.UserResponse{Status: http.StatusOK, Message: "success", Data: &fiber.Map{"data": user}})
 }
+
+
+func EditAUser(c *fiber.Ctx) error {
+    ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+    userId := c.Params("userId")
+    var user models.User
+    defer cancel()
+
+    objId, _ := primitive.ObjectIDFromHex(userId)
+
+    //validate the request body
+    if err := c.BodyParser(&user); err != nil {
+        return c.Status(http.StatusBadRequest).JSON(responses.UserResponse{Status: http.StatusBadRequest, Message: "error", Data: &fiber.Map{"data": err.Error()}})
+    }
+
+    //use the validator library to validate required fields
+    if validationErr := validate.Struct(&user); validationErr != nil {
+        return c.Status(http.StatusBadRequest).JSON(responses.UserResponse{Status: http.StatusBadRequest, Message: "error", Data: &fiber.Map{"data": validationErr.Error()}})
+    }
+
+    update := bson.M{"name": user.Name, "location": user.Location, "title": user.Title}
+
+    result, err := userCollection.UpdateOne(ctx, bson.M{"id": objId}, bson.M{"$set": update})
+
+    if err != nil {
+        return c.Status(http.StatusInternalServerError).JSON(responses.UserResponse{Status: http.StatusInternalServerError, Message: "error", Data: &fiber.Map{"data": err.Error()}})
+    }
+    //get updated user details
+    var updatedUser models.User
+    if result.MatchedCount == 1 {
+        err := userCollection.FindOne(ctx, bson.M{"id": objId}).Decode(&updatedUser)
+
+        if err != nil {
+            return c.Status(http.StatusInternalServerError).JSON(responses.UserResponse{Status: http.StatusInternalServerError, Message: "error", Data: &fiber.Map{"data": err.Error()}})
+        }
+    }
+
+    return c.Status(http.StatusOK).JSON(responses.UserResponse{Status: http.StatusOK, Message: "success", Data: &fiber.Map{"data": updatedUser}})
+}

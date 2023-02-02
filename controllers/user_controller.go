de8f3920249af180cf62f79e0197027663c6c4b4
Akinkunmi <akinkunmiisrael41@gmail.com>
2023-02-02 21:11:23 +0100

    get all users

--- controllers/user_controller.go
+++ controllers/user_controller.go
@@ -128,3 +128,32 @@ func DeleteAUser(c *fiber.Ctx) error {
         responses.UserResponse{Status: http.StatusOK, Message: "success", Data: &fiber.Map{"data": "User successfully deleted!"}},
     )
 }
+
+
+
+func GetAllUsers(c *fiber.Ctx) error {
+    ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+    var users []models.User
+    defer cancel()
+
+    results, err := userCollection.Find(ctx, bson.M{})
+
+    if err != nil {
+        return c.Status(http.StatusInternalServerError).JSON(responses.UserResponse{Status: http.StatusInternalServerError, Message: "error", Data: &fiber.Map{"data": err.Error()}})
+    }
+
+    //reading from the db in an optimal way
+    defer results.Close(ctx)
+    for results.Next(ctx) {
+        var singleUser models.User
+        if err = results.Decode(&singleUser); err != nil {
+            return c.Status(http.StatusInternalServerError).JSON(responses.UserResponse{Status: http.StatusInternalServerError, Message: "error", Data: &fiber.Map{"data": err.Error()}})
+        }
+
+        users = append(users, singleUser)
+    }
+
+    return c.Status(http.StatusOK).JSON(
+        responses.UserResponse{Status: http.StatusOK, Message: "success", Data: &fiber.Map{"data": users}},
+    )
+}
